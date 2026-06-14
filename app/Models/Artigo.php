4c3b29c6e3edc9cdde58bfd229214f0c05b86722
Eliasggabr <elias.menezes1@aluno.ce.gov.br>
2026-06-14 15:34:58 -0300

    Configurando os models e o middleware

--- app/Models/Artigo.php
+++ app/Models/Artigo.php
@@ -2,9 +2,18 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Artigo extends Model
 {
-    //
+    use HasFactory;
+
+    protected $table = 'artigos';
+
+    protected $fillable = [
+        'titulo',
+        'autor',
+        'conteudo'
+    ];
 }
